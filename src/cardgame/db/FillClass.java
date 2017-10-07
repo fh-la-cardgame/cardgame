8d836141a981fa87d31179f091c366f7364c3036
User <User@Amadeus.home>
2017-10-07 18:39:22 +0200

    Anlegen von FillClass damit leere Packages hochgeladen werden.

---
+++ src/cardgame/db/FillClass.java
@@ -0,0 +1,12 @@
+/*
+ * Klasse dient als Füllklasse, damit die leeren Packages hochgeladen werden.
+ */
+package cardgame.db;
+
+/**
+ *
+ * @author HortensiaX
+ */
+public class FillClass {
+    
+}
---
+++ src/cardgame/logic/FillClass.java
@@ -0,0 +1,12 @@
+/*
+ * Klasse dient als Füllklasse, damit die leeren Packages hochgeladen werden.
+ */
+package cardgame.logic;
+
+/**
+ *
+ * @author HortensiaX
+ */
+public class FillClass {
+    
+}
