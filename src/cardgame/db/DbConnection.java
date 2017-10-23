004b7859c05dc7a61b9f40319d0380a2e7cf99ab
HortensiaX <s-iyalov@haw-landshut.de>
2017-10-23 14:52:12 +0200

    Connection Klasse erstellt

---
+++ src/cardgame/db/DbConnection.java
@@ -0,0 +1,26 @@
+package cardgame.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Baut eine Verbindung zu einer Postgresql Datenbank auf.
+ * @author BishaThan
+ */
+public class DbConnection {
+    /**
+     * Baut eine DB-Verbindung auf
+     * @return Verbindung
+     * @throws SQLException 
+     */
+    public static Connection getPostgresConnection() throws SQLException, ClassNotFoundException{
+        String user = "";
+        String pw = "";
+        String host = "";
+        Class.forName("org.postgresql.Driver");
+        return DriverManager
+            .getConnection("jdbc:postgresql://" + host,
+            user, pw);
+    }
+}
