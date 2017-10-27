67d7c3848378bd9c6ffede57abcb4f944a9c9191
davidono <david.onofrasch96@gmail.com>
2017-10-27 08:50:48 +0200

    Auslagerung von Code in Hilfsmethoden in DBCard

--- src/cardgame/db/DbCard.java
+++ src/cardgame/db/DbCard.java
@@ -33,38 +33,24 @@ public class DbCard {
     
     /** Gibt eine Liste aller Karten zurueck
      * @return List<Card> Eine Liste aller Karten, ausgelesen aus der DB
-     * @throws IllegalArgumentException Wenn die Enums, die aus der DB ausgelesen wurden, nicht existent sind
      */
     public List<Card> getAllCards(){
         List<Card> l = new LinkedList<>();
         
         try {
             c = DbConnection.getPostgresConnection();
-            /*
-             * Ausfuehren des Selects um alle notwendigen Infos aus Gamecard zu beziehen
-             */
+             //Ausfuehren des Selects um alle notwendigen Infos aus Gamecard zu beziehen.
             pst = c.prepareStatement("Select g.\"gid\", g.\"name\", g.\"description\", g.\"monster_type\", g.\"atk\", g.\"shield_curr\","+" "
             		+ "g.\"shield_max\", g.\"evo_shield_curr\", g.\"evo_shield_max\" from public.\"Gamecard\" g order by g.\"gid\"");
             rs = pst.executeQuery();
             
             while(rs.next()){
-                /*
-                 * Da das ResultSet nur Strings statt Enums liefert, werden die Strings mittels switch - Anweisung
-                 * dem zugehoerigem Enum zugeordnet.
-                 */
+                 // Da das ResultSet nur Strings statt Enums liefert, werden die Strings mittels switch - Anweisung
+                 // in der Hilfsmethode StringToType dem zugehoerigem Enum zugeordnet.
                 Type type = null;
-                switch(rs.getString(4)){
-                case "Daemon":  type = Type.daemon;		break;
-                case "Pflanze": type = Type.pflanze;	break;
-                case "Fairy": 	type = Type.fairy;		break;
-                case "Human": 	type = Type.human;		break;
-                case "Orc":		type = Type.orc;		break;
-                case "Dragon": 	type = Type.dragon;		break;
-                default: throw new IllegalArgumentException("Kein existenter Type(Enum)");
-                }
-                /*
-                 * Ausfuerung des Joins(ueber Card_Effekt) um die Effekte einer Karte auszulesen
-                 */
+                type = stringToType(rs.getString(4));
+                
+                 //Ausfuerung des Joins(ueber Card_Effekt) um die Effekte einer Karte auszulesen.
                 PreparedStatement join = c.prepareStatement("select e.eid, e.description, e.effect_type, e.effect_number, c_e.shield"+""
                 		+ " from \"Effecte\" e, \"Card_Effect\" c_e, \"Gamecard\" g where g.gid = c_e.gid and c_e.eid = e.eid and g.gid = "
                 		+ rs.getInt(1));
@@ -73,19 +59,11 @@ public class DbCard {
                 EffectType effect = null;
                 Effect[] effects = new Effect[MAX_EFFEKTS];
                 int i = 0;
-                /*
-                 * Da das ResultSet nur Strings statt Enums liefert, werden die Strings mittels switch - Anweisung
-                 * dem zugehoerigem Enum zugeordnet.
-                 */
+
                 while(rs2.next()){
-                	switch(rs2.getString(3)){
-                    case "destroy": effect = EffectType.destroy; 					break;
-                    case "substraction_one": effect = EffectType.substraction_one; 	break;
-                    case "substraction_all": effect = EffectType.subtraction_all; 	break;
-                    case "addition_one": effect = EffectType.addition_one; 			break;
-                    case "addition_all": effect = EffectType.addition_all; 			break;
-                    default: throw new IllegalArgumentException("Kein existenter EffectType(Enum)");
-                    }
+                    //Da das ResultSet nur Strings statt Enums liefert, werden die Strings mittels switch - Anweisung
+                    // in der Hilfsmethode stringToEffectType dem zugehoerigem Enum zugeordnet.
+                	effect = stringToEffectType(rs2.getString(3));
                 	effects[i] = new Effect(rs2.getInt(1), rs2.getString(2), effect);
                 	i++;
                 }
@@ -98,8 +76,43 @@ public class DbCard {
             Logger.getLogger(DbCard.class.getName()).log(Level.SEVERE, null, ex);
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(DbCard.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
+        } 
         return l;
      }
+    
+    
+    /** Ordnet einem String einen Enum zu
+     * @return Type Gibt den zum String gehoerenden Enum zurueck
+     * @throws new IllegalArgumentException Wenn der String keinem Enum zugeordnet werden kann
+     */
+    private Type stringToType(String string){
+    	Type type = null;
+    	switch(string){
+        case "Daemon":  type = Type.daemon;		break;
+        case "Pflanze": type = Type.pflanze;	break;
+        case "Fairy": 	type = Type.fairy;		break;
+        case "Human": 	type = Type.human;		break;
+        case "Orc":		type = Type.orc;		break;
+        case "Dragon": 	type = Type.dragon;		break;
+        default: throw new IllegalArgumentException("Kein existenter Type(Enum)");
+        }
+    	return type;
+    }
+    
+    /** Ordnet einem String einen Enum zu
+     * @return EffectType Gibt den zum String gehoerenden Enum zurueck 
+     * @throws new IllegalArgumentException Wenn der String keinem Enum zugeordnet werden kann
+     */
+    private EffectType stringToEffectType(String string){
+    	EffectType effect = null;
+    	switch(string){
+        case "destroy": effect = EffectType.destroy; 					break;
+        case "substraction_one": effect = EffectType.substraction_one; 	break;
+        case "substraction_all": effect = EffectType.subtraction_all; 	break;
+        case "addition_one": effect = EffectType.addition_one; 			break;
+        case "addition_all": effect = EffectType.addition_all; 			break;
+        default: throw new IllegalArgumentException("Kein existenter EffectType(Enum)");
+        }
+    	return effect;
+    }
 }
