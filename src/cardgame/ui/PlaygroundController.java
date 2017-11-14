9d05342b06a1e4f1672ba1a0ba75f5a39d2bfe55
BishaThan <s-iyalov@haw-landshut.de>
2017-11-14 17:09:39 +0100

    Battlefield deleted

---
+++ src/cardgame/ui/PlaygroundController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cardgame.ui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+
+/**
+ * FXML Controller class
+ *
+ * @author BishaThan
+ */
+public class PlaygroundController implements Initializable {
+
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    
+    void main(String... test){
+    }
+    
+}
--- src/cardgame/ui/PlaygroundWindow.java
+++ src/cardgame/ui/PlaygroundWindow.java
@@ -5,11 +5,14 @@
  */
 package cardgame.ui;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -17,28 +20,20 @@ import javafx.stage.Stage;
  *
  * @author BishaThan
  */
-public class Playground extends Application {
+public class PlaygroundWindow extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+        try{
+        AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Playground.fxml"));
+
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
+        Scene scene = new Scene(root, 500, 550);
         primaryStage.setScene(scene);
-        primaryStage.show();
+        primaryStage.show();}
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 
     /**
