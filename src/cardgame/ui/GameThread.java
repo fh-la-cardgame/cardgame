a5aa2f82e1741629ce14d53c7526470c2bc931bf
Johannes <johannes.steinleitner@googlemail.com>
2018-05-11 18:52:39 +0200

    Game Thread one Player

--- src/cardgame/ui/GameThread.java
+++ src/cardgame/ui/GameThread.java
@@ -1,6 +1,7 @@
 package cardgame.ui;
 
 import cardgame.ai.KiPlayer;
+import cardgame.ai.RandomPlayer;
 import cardgame.ai.TestPlayer;
 import cardgame.classes.GameEndException;
 import cardgame.logic.Game;
@@ -18,44 +19,21 @@ public class GameThread extends Thread {
     public void run() {
         try {
             KiPlayer p1 = new TestPlayer(game, 1);
-            KiPlayer p2 = new TestPlayer(game, 2);
+            //KiPlayer p2 = new TestPlayer(game, 2);
             int k = 0;
-            while (game.isGameRunning()) {
                 game.changePlayer(p1.getId());
                 try {
                     game.getMyField(p1.getId()).addCard();
                 } catch (GameEndException e) {
-                    game.setPlayerWon(p2.getId());
+                    game.setPlayerWon(2);
                     game.setGameEnd(true);
-                    break;
+                    return;
                 }
                 try {
                     p1.yourTurn();
-                    if (k == 0)
-                        System.out.println(game.getCardsOnHand(p1.getId()).size() + " " + game.getCardsOnHand(p2.getId()).size());
-
                 } catch (LogicException e) {
                     System.out.println(e);
                 }
-                if (!game.isGameRunning()) {
-                    break;
-                }
-                game.changePlayer(p2.getId());
-                try {
-                    game.getMyField(p2.getId()).addCard();
-                } catch (GameEndException e) {
-                    game.setPlayerWon(p1.getId());
-                    game.setGameEnd(true);
-                    break;
-                }
-                try {
-                    p2.yourTurn();
-                } catch (LogicException e) {
-                    System.out.println(e);
-                }
-                k++;
-            }
-            System.out.println("Spieler " + game.getMyField(game.getPlayerWon()).getPlayer().getName() + " hat gewonnen");
         } catch (LogicException ex) {
             System.out.println(ex);
         }
